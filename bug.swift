5707dda0d0817a868b136498a8dfe8cda4d4423c
BugHunter-X <askzenai@gmail.com>
2025-02-09 01:50:04 +0100

    Add bug.swift file

---
+++ bug.swift
@@ -0,0 +1,3 @@
+let arr = [1, 2, 3]
+let lastElement = arr.last
+//The compiler may not detect errors during development if an array is accessed out of bounds, especially with optional unwrapping, leading to unexpected crashes during runtime.
